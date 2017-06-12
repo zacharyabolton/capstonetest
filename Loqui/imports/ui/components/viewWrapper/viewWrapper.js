a538410f715be69f80c806215b2e0c23340f169f
Zac Bolton <zacbolton2129@gmail.com>
2017-06-12 18:56:45 -0300

    Improving viewBtn w/reactive_var

--- Loqui/imports/ui/components/viewWrapper/viewWrapper.js
+++ Loqui/imports/ui/components/viewWrapper/viewWrapper.js
@@ -10,18 +10,28 @@ import './viewWrapper_components/list/list.js';
 Template.viewWrapper.onCreated(function() {
   const instance = this;
   instance.toggleDisplay = new ReactiveVar(true);
-  instance.calOrList = new ReactiveVar("Calendar");
+  instance.calOrList = new ReactiveVar("Switch to list view");
 });
 
 Template.viewWrapper.helpers({
   toggleDisplay() {
     const instance = Template.instance();
     return instance.toggleDisplay.get();
+  },
+  calOrList() {
+  	const instance = Template.instance();
+  	return instance.calOrList.get();
   }
 });
 
 Template.viewWrapper.events({
   "click #viewBtn": function(event, instance) {
-  instance.toggleDisplay.set(!instance.toggleDisplay.get());
+	  instance.toggleDisplay.set(!instance.toggleDisplay.get());
+	  var calView = instance.toggleDisplay.get();
+	  if(calView === true){
+	  	instance.calOrList.set("Switch to list view");
+	  }else{
+	  	instance.calOrList.set("Switch to calendar view");
+  	}
   }
 });

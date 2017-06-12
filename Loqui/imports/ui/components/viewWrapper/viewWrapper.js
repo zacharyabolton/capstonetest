7bcd7c509c64952ebb540944523aa8a3d4288e2f
Zac Bolton <zacbolton2129@gmail.com>
2017-06-12 18:45:47 -0300

    Implementing toggleDisplay w/reactive_var

--- Loqui/imports/ui/components/viewWrapper/viewWrapper.js
+++ Loqui/imports/ui/components/viewWrapper/viewWrapper.js
@@ -1,4 +1,27 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
 import './viewWrapper.html';
 
 import './viewWrapper_components/calendar/calendar.js';
 import './viewWrapper_components/list/list.js';
+
+Template.viewWrapper.onCreated(function() {
+  const instance = this;
+  instance.toggleDisplay = new ReactiveVar(true);
+  instance.calOrList = new ReactiveVar("Calendar");
+});
+
+Template.viewWrapper.helpers({
+  toggleDisplay() {
+    const instance = Template.instance();
+    return instance.toggleDisplay.get();
+  }
+});
+
+Template.viewWrapper.events({
+  "click #viewBtn": function(event, instance) {
+  instance.toggleDisplay.set(!instance.toggleDisplay.get());
+  }
+});

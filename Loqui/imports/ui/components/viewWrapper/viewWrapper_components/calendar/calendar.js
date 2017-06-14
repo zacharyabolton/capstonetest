894b7cc7a69c690c5204318e17ea2b621e607dfb
Zac Bolton <zacbolton2129@gmail.com>
2017-06-14 19:12:11 -0300

    adding aldeed:collection2/aldeed:simple-schema & implementing schema and events collection

--- Loqui/imports/ui/components/viewWrapper/viewWrapper_components/calendar/calendar.js
+++ Loqui/imports/ui/components/viewWrapper/viewWrapper_components/calendar/calendar.js
@@ -1,7 +1,25 @@
 import {Template} from 'meteor/templating';
 
+import {Events} from '../../../../../api/events/events.js';
+
 import './calendar.html';
 
+Template.calendar.onCreated( () => {
+  let template = Template.instance();
+  template.subscribe( 'events' );
+});
+
 Template.calendar.onRendered( () => {
-  $( '#events-calendar' ).fullCalendar();
+  $( '#events-calendar' ).fullCalendar({
+  	events( start, end, timezone, callback ) {
+      let data = Events.find().fetch().map( ( event ) => {
+        event.editable = !isPast( event.start );
+        return event;
+      });
+
+      if ( data ) {
+        callback( data );
+      }
+    }
+  });
 });

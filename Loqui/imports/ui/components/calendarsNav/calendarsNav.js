6c49094a69e9c63d4c41e1d9c844c8a44951142d
Zac Bolton <zacbolton2129@gmail.com>
2017-06-24 19:14:47 -0300

    Adding reactive dropdown department btn

--- Loqui/imports/ui/components/calendarsNav/calendarsNav.js
+++ Loqui/imports/ui/components/calendarsNav/calendarsNav.js
@@ -1,1 +1,34 @@
+import {Meteor} from 'meteor/meteor';
+import {Template} from 'meteor/templating';
+import {Tracker} from 'meteor/tracker';
+import {Session} from 'meteor/session';
+
+import {Events} from '../../../api/events/events.js';
+
 import './calendarsNav.html';
+
+Template.calendarsNav.onCreated( () => {
+  let template = Template.instance();
+  template.subscribe( 'events' );
+});
+
+Template.calendarsNav.helpers({
+	distinctDepartments() {
+    var distinctDepartments = _.uniq(Events.find({}, {
+		  sort: {department: 1}, fields: {department: true}
+		}).fetch().map(function(x) {
+		  return x.department;
+		}), true);
+		return distinctDepartments;
+  },
+});
+
+Template.calendarsNav.events({
+	'click .departmentDropDownItem'(){
+		var depFilter = Events.find({}, {"department": this});
+
+		var test = depFilter.title;
+
+		console.log(test);
+	}
+})
